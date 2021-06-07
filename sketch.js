5ef6eb60301d2f322e391722053ec41ea54fec58
SakshamJain17 <71916941+SakshamJain17@users.noreply.github.com>
2021-06-07 11:59:46 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,10 +4,11 @@ const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
 var backImg, backgroundImg, craft1Img, spaceCraft,spaceCraft1, bombImg, bomb, explosionImg, playImg;
-var bombGroup, gameState, craft2Img,craft3Img,craft4Img, finishImg, finish, homeP, homeImg, playB, boomS;
-var clickS, explosion1Img;
+var bombGroup, gameState = "start", craft2Img,craft3Img,craft4Img, finishImg, finish, homeP;
+var homeImg, playB, boomS, feedback_img, feedback;
+var clickS, explosion1Img, coinImg, coin;
 var level = 1;
-var finishGroup;
+var finishGroup, CoinGroup;
 
 function preload(){
     backImg = loadImage("images/background.jpg");
@@ -21,12 +22,15 @@ function preload(){
     finishImg = loadImage("images/finish.png");
     playImg = loadImage("images/play.png");
     homeImg = loadImage("images/homeP.jpg");
+    coinImg = loadImage("images/coin.png");
+    feedback_img = loadImage("images/feedback1.jpg");
     boomS = loadSound("sound/boom.wav");
     clickS = loadSound("sound/click.mp3");
 }
 
 function setup(){
     createCanvas(windowWidth/2.5, windowHeight-10);
+    
     
 backgroundImg = createSprite(windowWidth/4.4,height/2,100,100);
 backgroundImg.addImage(backImg);
@@ -42,6 +46,7 @@ spaceCraft.addImage("explosion", explosionImg);
 //spaceCraft.rotation = -90;
 bombGroup = new Group();
 finishGroup = new Group(); 
+CoinGroup = new Group(); 
 
 homeP = createSprite(width/2,height/2,100,100);
 homeP.addImage(homeImg);
@@ -60,21 +65,25 @@ spaceCraft2.addImage(craft2Img);
 spaceCraft2.scale = 0.6;
 spaceCraft2.rotation = -25;
 
-spaceCraft3 = createSprite(width/4.5,600,100,100);
+spaceCraft3 = createSprite(width/4,570,100,100);
 spaceCraft3.addImage(craft3Img);
-spaceCraft3.scale = 0.8;
+spaceCraft3.scale = 0.9;
 spaceCraft3.rotation = 25;
 
-spaceCraft4 = createSprite(width/1.3,600,100,100);
+spaceCraft4 = createSprite(width/1.3,570,100,100);
 spaceCraft4.addImage(craft4Img);
 spaceCraft4.scale = 0.6;
 spaceCraft4.rotation = -25;
         
+feedback = createSprite(width/2,height/2,100,100);
+feedback.addImage(feedback_img);
+feedback.scale = 1.3;
+feedback.visible = false;
     }
 
 function draw(){
     background("red");
-    if(mousePressedOver(playB)){
+    if(mousePressedOver(playB)&& gameState === "start"){
         homeP.destroy();
         spaceCraft1.destroy();
         spaceCraft2.destroy();
@@ -89,9 +98,53 @@ function draw(){
     if(gameState === "play" && level === 1){
         
         if(keyDown(LEFT_ARROW)){
-            spaceCraft.x = spaceCraft.x-50;
+            spaceCraft.x = spaceCraft.x-30;
         }
        
+        if(keyDown(RIGHT_ARROW)){
+            spaceCraft.x = spaceCraft.x+30;
+        }
+        if(backgroundImg.y > 700){
+            backgroundImg.y = backgroundImg.height/5;
+        }
+        if(spaceCraft.x < 100){
+            spaceCraft.x = 100;
+        }
+        if(spaceCraft.x > width - 100){
+            spaceCraft.x = width - 100;
+        }
+        //clickS.stop();
+       //clickS.destroy();
+        spawnBomb();
+        spawnFinish();
+        spawnCoin();
+        if(finishGroup.isTouching(spaceCraft)){
+            level = 2;
+            console.log("level 1 ended")
+        }
+        if(CoinGroup.isTouching(spaceCraft)){
+            CoinGroup.destroyEach();
+            clickS.play();
+        
+        }
+        if(bombGroup.isTouching(spaceCraft)){
+            spaceCraft.changeAnimation("explosion");
+            spaceCraft.scale = 1.5;
+            boomS.play();
+
+            //boomS.hide();
+            //spaceCraft.scale = 1;
+              // spaceCraft.changeImage( explosionImg);
+            gameState = "end";
+        }
+        
+    }
+
+    if(gameState === "play" && level === 2){
+        spaceCraft.changeImage("2")
+        if(keyDown(LEFT_ARROW)){
+            spaceCraft.x = spaceCraft.x-50;
+        }
         if(keyDown(RIGHT_ARROW)){
             spaceCraft.x = spaceCraft.x+50;
         }
@@ -108,9 +161,11 @@ function draw(){
        //clickS.destroy();
         spawnBomb();
         spawnFinish();
+        spawnCoin();
+
         if(finishGroup.isTouching(spaceCraft)){
-            level = 2;
-            console.log("level 1 ended")
+            level = 3;
+            console.log("level 2 ended")
         }
         if(bombGroup.isTouching(spaceCraft)){
             spaceCraft.changeAnimation("explosion");
@@ -123,9 +178,8 @@ function draw(){
         }
         
     }
-
-    if(gameState === "play" && level === 2){
-        spaceCraft.changeImage("2")
+    if(gameState === "play" && level === 3){
+        spaceCraft.changeImage("3");
         if(keyDown(LEFT_ARROW)){
             spaceCraft.x = spaceCraft.x-50;
         }
@@ -146,6 +200,51 @@ function draw(){
        //clickS.destroy();
         spawnBomb();
         spawnFinish();
+        spawnCoin();
+
+        if(finishGroup.isTouching(spaceCraft)){
+            level = 4;
+            console.log("level 3 ended")
+        }
+        if(bombGroup.isTouching(spaceCraft)){
+            spaceCraft.changeAnimation("explosion");
+            spaceCraft.scale = 1.5;
+            boomS.play();
+            //boomS.hide();
+            //spaceCraft.scale = 1;
+              // spaceCraft.changeImage( explosionImg);
+            gameState = "end";
+        }
+        
+    }
+    if(gameState === "play" && level === 4){
+        spaceCraft.changeImage("4");
+        if(keyDown(LEFT_ARROW)){
+            spaceCraft.x = spaceCraft.x-50;
+        }
+       
+        if(keyDown(RIGHT_ARROW)){
+            spaceCraft.x = spaceCraft.x+50;
+        }
+        if(backgroundImg.y > 700){
+            backgroundImg.y = backgroundImg.height/5;
+        }
+        if(spaceCraft.x < 100){
+            spaceCraft.x = 100;
+        }
+        if(spaceCraft.x > width - 100){
+            spaceCraft.x = width - 100;
+        }
+        //clickS.stop();
+       //clickS.destroy();
+        spawnBomb();
+        spawnFinish();
+        spawnCoin();
+
+        // if(finishGroup.isTouching(spaceCraft)){
+        //     level = 3;
+        //     console.log("level 2 ended")
+        // }
         if(bombGroup.isTouching(spaceCraft)){
             spaceCraft.changeAnimation("explosion");
             spaceCraft.scale = 1.5;
@@ -159,6 +258,8 @@ function draw(){
     }
     else if(gameState === "end"){
         bombGroup.destroyEach();
+        finishGroup.destroyEach();
+        CoinGroup.destroyEach();
         backgroundImg.velocityY = 0;
         
     }
@@ -177,6 +278,16 @@ function draw(){
         bombGroup.add(bomb);
        }    
     }
+    function spawnCoin(){
+        if(frameCount %80 === 0){
+            coin = createSprite(random(100,width-100),0,100);
+            coin.addImage(coinImg);
+            coin.scale = 0.04;
+            coin.velocityY = 10;
+            coin.lifetime = 500;
+            CoinGroup.add(coin);
+           }
+    }
     function spawnFinish(){
         if(frameCount %100 === 0){
         finish = createSprite(width/2,height/7,150,100);
@@ -185,29 +296,9 @@ function draw(){
         finish.lifetime = 500;
         finish.velocityY = 10;
         finishGroup.add(finish);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         }
     }
+        
 
 
 
